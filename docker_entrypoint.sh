e8f0e992d0142cbb71a920aa304c37d8c9af9afd
Remco Ros <remcoros@live.nl>
2024-02-04 14:03:16 +0000

    suppress output of tee

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -40,19 +40,19 @@ case "$(yq -r '.server.type' /root/data/start9/config.yaml)" in
   echo "Configuring Sparrow for Bitcoin Core"
   BITCOIND_USER=$(yq -r '.server.user' /root/data/start9/config.yaml)
   BITCOIND_PASS=$(yq -r '.server.password' /root/data/start9/config.yaml)
-  cat /config/.sparrow/config | jq '.serverType = "BITCOIN_CORE"' | tee /config/.sparrow/config
-  cat /config/.sparrow/config | jq '.coreServer = "http://127.0.0.1:8332"' | tee /config/.sparrow/config
-  cat /config/.sparrow/config | jq '.coreAuthType = "USERPASS"' | tee /config/.sparrow/config
-  cat /config/.sparrow/config | jq ".coreAuth = \"${BITCOIND_USER}:${BITCOIND_PASS}\"" | tee /config/.sparrow/config
+  cat /config/.sparrow/config | jq '.serverType = "BITCOIN_CORE"' | tee /config/.sparrow/config 1>/dev/null
+  cat /config/.sparrow/config | jq '.coreServer = "http://127.0.0.1:8332"' | tee /config/.sparrow/config 1>/dev/null
+  cat /config/.sparrow/config | jq '.coreAuthType = "USERPASS"' | tee /config/.sparrow/config 1>/dev/null
+  cat /config/.sparrow/config | jq ".coreAuth = \"${BITCOIND_USER}:${BITCOIND_PASS}\"" | tee /config/.sparrow/config 1>/dev/null
   ;;
 "electrs")
   echo "Configuring Sparrow for Electrs"
-  cat /config/.sparrow/config | jq '.serverType = "ELECTRUM_SERVER"' | tee /config/.sparrow/config
-  cat /config/.sparrow/config | jq '.coreServer = "tcp://127.0.0.1:50001"' | tee /config/.sparrow/config
+  cat /config/.sparrow/config | jq '.serverType = "ELECTRUM_SERVER"' | tee /config/.sparrow/config 1>/dev/null
+  cat /config/.sparrow/config | jq '.coreServer = "tcp://127.0.0.1:50001"' | tee /config/.sparrow/config 1>/dev/null
   ;;
 "public")
   echo "Configuring Sparrow for Public electrum server"
-  cat /config/.sparrow/config | jq '.serverType = "PUBLIC_ELECTRUM_SERVER"' | tee /config/.sparrow/config
+  cat /config/.sparrow/config | jq '.serverType = "PUBLIC_ELECTRUM_SERVER"' | tee /config/.sparrow/config 1>/dev/null
   ;;
 *)
   echo "Custom server selected, not configuring Sparrow"
@@ -63,13 +63,12 @@ case "$(yq -r '.proxy.type' /root/data/start9/config.yaml)" in
 "tor")
   echo "Configuring Sparrow for Tor"
   EMBASSY_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
-  BITCOIND_USER=$(yq -r '.server.type' /root/data/start9/config.yaml)
-  cat /config/.sparrow/config | jq '.useProxy = true' | tee /config/.sparrow/config
-  cat /config/.sparrow/config | jq ".proxyServer = \"${EMBASSY_IP}:9050\"" | tee /config/.sparrow/config
+  cat /config/.sparrow/config | jq '.useProxy = true' | tee /config/.sparrow/config 1>/dev/null
+  cat /config/.sparrow/config | jq ".proxyServer = \"${EMBASSY_IP}:9050\"" | tee /config/.sparrow/config 1>/dev/null
   ;;
 "none")
   echo "Configuring Sparrow for 'no proxy'"
-  cat /config/.sparrow/config | jq '.useProxy = false' | tee /config/.sparrow/config
+  cat /config/.sparrow/config | jq '.useProxy = false' | tee /config/.sparrow/config 1>/dev/null
   ;;
 *)
   echo "Custom proxy selected, not configuring Sparrow"
