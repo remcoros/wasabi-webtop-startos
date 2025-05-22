4a7000f3542d1921487868e61036ddd52bc04c70
Remco Ros <remcoros@live.nl>
2025-05-22 17:35:17 +0000

    tweaks

--- startos/init/index.ts
+++ startos/init/index.ts
@@ -6,11 +6,13 @@ import { actions } from '../actions'
 import { restoreInit } from '../backups'
 import { config } from '../actions/config'
 
-const setupPostInstall = sdk.setupOnInstall(async (effects) => {
-  // require the config action to run once, to have a password for the ui set
-  await sdk.action.createOwnTask(effects, config, 'critical', {
-    reason: 'Configure default settings',
-  })
+const setupPostInstall = sdk.setupOnInit(async (effects, kind) => {
+  if (kind == 'install') {
+    // require the config action to run once, to have a password for the ui set
+    await sdk.action.createOwnTask(effects, config, 'critical', {
+      reason: 'Configure default settings',
+    })
+  }
 })
 
 export const init = sdk.setupInit(
